18776c4cd026baef7dda650ae5441ba5101ec88c
voldemoriarty <mamin.bee16seecs@seecs.edu.pk>
2019-11-16 10:57:12 +0500

    restructure directories

--- risac_soc/simulation/modelsim/rtl_work/risac_soc/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity risac_soc is
-    port(
-        clock50         : in     vl_logic;
-        reset_n         : in     vl_logic;
-        ledr            : out    vl_logic_vector(9 downto 0)
-    );
-end risac_soc;
--- risac_soc/simulation/modelsim/rtl_work/risac_soc_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity risac_soc_tb is
-end risac_soc_tb;
