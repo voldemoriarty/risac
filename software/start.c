153392f8cce29e3027f52b6a31a5ccf982c9214b
saad <mamin.bee16seecs@seecs.edu.pk>
2019-11-23 00:18:16 +0500

    fixed bug in startup code

--- software/start.c
+++ software/start.c
@@ -1,7 +1,5 @@
 extern void _start();
 
-extern int * _stack_pointer;
-
 // the actual entry point
 // for now, only call main
 __attribute__((section (".text.init")))
@@ -9,6 +7,6 @@ void _actual_start () {
 	// setup the stack
 	// must needed for both stdlib and newlib versions
 	// since newlib assumes that stack has already been setup
-	__asm("mv sp, %[value]"::[value]"r"(_stack_pointer));
+	__asm("la sp, _stack_pointer");
 	_start();
 }
