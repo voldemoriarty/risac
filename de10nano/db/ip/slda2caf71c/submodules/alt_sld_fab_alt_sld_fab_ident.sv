6ccdaf6a29f5f28d4932cebda37e32d18bd510ae
saad <mamin.bee16seecs@seecs.edu.pk>
2019-11-28 01:12:43 +0500

    new synth

--- de10nano/db/ip/slda2caf71c/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ de10nano/db/ip/slda2caf71c/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : dd7ab549acbe9b6aa03e
+//   DESIGN_HASH   : 6b1b99d0da6542992774
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hdd7ab549acbe9b6aa03e };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h6b1b99d0da6542992774 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
