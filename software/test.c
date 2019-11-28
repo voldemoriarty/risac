c2cabee058b7368adc8214d04cc814bfa5b6da64
saad <mamin.bee16seecs@seecs.edu.pk>
2019-11-28 13:28:55 +0500

    check small string bug

--- software/test.c
+++ software/test.c
@@ -6,11 +6,7 @@
  * 3. use objcopy to get an intel hex format file
  */
 
-inline void delay () {
-	for (volatile unsigned i = 0; i < 100000; ++i);
-}
-
-inline void _puts (volatile char* uart, const char* msg, const int len) {
+void _puts (volatile char* uart, const char* msg, const int len) {
 	volatile unsigned int *status = (volatile unsigned int *)uart;
 	for (int i = 0; i < len; ++i) {
 		while (status[1] >> 16 == 0);
@@ -21,9 +17,9 @@ inline void _puts (volatile char* uart, const char* msg, const int len) {
 // the (fake) entry point
 int main () {
 	volatile char *leds = (volatile char *)0x2000000;
-	*leds = 0x55;
+	*leds = 0x50;
 	volatile char *uart = (volatile char *)0x1000000;
-	const char msg[] = "Howdy dowdy doo daa\r\n";
+	const char msg[] = "Welcome dr foo bar\r\n";
 	_puts(uart, msg, sizeof(msg));
 	while (1);
 }
