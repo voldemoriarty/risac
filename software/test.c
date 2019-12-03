8b8696ca8ca4ab0e586ae45dc15bb990120fd935
saad <mamin.bee16seecs@seecs.edu.pk>
2019-12-03 21:54:30 +0500

    added a more complex program

--- software/test.c
+++ software/test.c
@@ -13,14 +13,50 @@ void _puts (volatile char* uart, const char* msg, const int len) {
 		*uart = msg[i];
 	}
 }
+/**
+ * Convert an integer to hexadecimal string
+ * buff must have atleast 8 bytes
+ */
+void _itoa (char *buff, int num) {
+	union {
+		char 	bytes[4];
+		int 	word;
+	} number;
+
+	number.word = num;
+
+	const char lut[] = "0123456789abcdef";
+
+	// first char is msn of msb
+	buff[0] = lut [number.bytes[3] >> 4];
+	buff[1] = lut [number.bytes[3] & 0xf];
+
+	buff[2] = lut [number.bytes[2] >> 4];
+	buff[3] = lut [number.bytes[2] & 0xf];
+
+	buff[4] = lut [number.bytes[1] >> 4];
+	buff[5] = lut [number.bytes[1] & 0xf];
+
+	buff[6] = lut [number.bytes[0] >> 4];
+	buff[7] = lut [number.bytes[0] & 0xf];
+}
 
 // the (fake) entry point
 int main () {
 	volatile char *leds = (volatile char *)0x2000000;
 	*leds = 0x50;
 	volatile char *uart = (volatile char *)0x1000000;
-	const char msg[] = "Howdy friend, how goes the hunt for souls?\r\n";
-	_puts(uart, msg, sizeof(msg));
+	
+	char buffer[10];
+
+	buffer[8] = '\r';
+	buffer[9] = '\n';
+	
+	for (int i = 0xff; i < 0xff + 2; ++i) {
+		_itoa(buffer, i);
+		_puts(uart, "0x", 2);
+		_puts(uart, buffer, 10);
+	}
 	while (1);
 }
 
